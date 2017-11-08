09881f47a5d5102670964bcc695997d5af88d764
FlamingGenius <livewiretech.reply.mail@gmail.com>
2017-11-08 14:30:12 -0800

    upload

---
+++ Coding Idle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Upgrade{
+	Clicker,
+	Scoreboard,
+	Solution,
+	UpgradeTable,
+}
+
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance{ set; get; }
+
+	[SerializeField]
+	public Dictionary<string,int> upgrades = new Dictionary<string, int> ();
+
+	[SerializeField]
+	public int units{ set; get; }
+
+	#region Monobehavior Callbacks
+	private void Awake(){
+		Instance = this;
+	}
+
+	private void Start(){
+		//Temporary solution till loading and saving are coded
+		LoadLocal();
+	}
+
+	private void Update(){
+		//Log debug information every 5secs //TEMP SOLUTION
+		if (Time.fixedTime % 5 == 0) {
+			Debug.Log ("Current Clicker level is: " + FetchUpgradeLevel (Upgrade.Clicker));
+			Debug.Log ("Current Scoreboard level is: " + FetchUpgradeLevel (Upgrade.Scoreboard));
+			Debug.Log ("Current Units" + units.ToString ());
+		}
+	}
+	#endregion
+
+	#region Saving and Loading
+	public void SaveLocal(){
+
+	}
+
+	public void LoadLocal(){
+		//Temp solution
+		upgrades.Add ("Clicker", 1);
+		upgrades.Add ("Scoreboard", 1);
+		upgrades.Add ("Solution", 1);
+		upgrades.Add ("UpgradeTable", 1);
+		units = 0;
+	}
+	#endregion
+	 
+	public void GainUnits(){
+		int level = FetchUpgradeLevel (Upgrade.Clicker);
+		switch (level) {
+		case 1:
+			units += 1;
+			break;
+		}
+	}
+
+	public int FetchUpgradeLevel(Upgrade upgradeType){
+		switch (upgradeType) {
+		case Upgrade.Clicker:
+			return upgrades ["Clicker"];
+		case Upgrade.Scoreboard:
+			return upgrades ["Scoreboard"];
+		case Upgrade.Solution:
+			return upgrades ["Solution"];
+		case Upgrade.UpgradeTable:
+			return upgrades ["UpgradeTable"];
+		default:
+			return 0;
+		}
+	}
+}
---
+++ Coding Idle/Assets/Scripts/UIManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	public static UIManager Instance{ set; get; }
+
+	public GameObject upgradePrefab;
+
+	public GameObject[] clickers;
+	public GameObject[] scoreboards;
+	public GameObject[] solution;
+	public GameObject[] upgradeTable;
+
+	private void Awake(){
+		Instance = this;
+	}
+
+	private void Update(){
+		UpdateUI ();
+	}
+
+	public void UpdateUI(){
+		#region Clicker
+		//Get level of clicker
+		int clickerLevel = GameManager.Instance.FetchUpgradeLevel(Upgrade.Clicker);
+		//Make sure correct clicker is always displayed
+		clickers[clickerLevel - 1].SetActive(true);
+		//Make sure no other clickers are displayed
+		foreach(GameObject go in clickers){
+			if(go != clickers[clickerLevel - 1]){
+				go.SetActive(false);
+			}
+		}
+		#endregion
+
+		#region Scoreboard
+		//Get level of scoreboard
+		int scoreboardLevel = GameManager.Instance.FetchUpgradeLevel (Upgrade.Scoreboard);
+		//Check if scoreboard has been bought
+		if (GameManager.Instance.FetchUpgradeLevel (Upgrade.Scoreboard) != 0) {
+			//Display correct scoreboard
+			scoreboards [scoreboardLevel - 1].SetActive (true);
+			//Make sure scoreboard is displaying correct value
+			scoreboards [scoreboardLevel - 1].GetComponentInChildren<Text> ().text = GameManager.Instance.units.ToString ();
+			//Make sure no other scoreboards are being displayed
+			foreach (GameObject go in scoreboards) {
+				if (go != scoreboards [scoreboardLevel - 1]) {
+					go.SetActive (false);
+				}
+			}
+		}
+		#endregion
+
+		#region Solution
+		int solutionLevel = GameManager.Instance.FetchUpgradeLevel(Upgrade.Solution);
+		solution[solutionLevel - 1].SetActive(true);
+		foreach(GameObject go in solution){
+			if(go != solution[solutionLevel - 1]){
+				go.SetActive(false);
+			}
+		}
+
+		#endregion
+
+		#region UpgradeTable
+		int upgradeTableLevel = GameManager.Instance.FetchUpgradeLevel(Upgrade.UpgradeTable);
+		upgradeTable[upgradeTableLevel - 1].SetActive(true);
+		foreach(GameObject go in upgradeTable){
+			if(go != upgradeTable[upgradeTableLevel - 1]){
+				go.SetActive(false);
+			}
+		}
+
+		//TODO
+		//Add a way to find the currently active table and add new upgrades prefab there
+
+		#endregion
+	}
+}
