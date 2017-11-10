bc2851faac3112d979593001d753792a3137dbc5
Shohanur Rahaman <shohan4556@gmail.com>
2017-11-10 19:44:26 +0600

    simple popUpWindow
    simple popup windows, let me know If you want this then I will step forward.

---
+++ Coding Idle/Assets/_Shohan/_Scripts/_Clicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Clicker : MonoBehaviour {
+
+	[SerializeField]
+	public int units{ set; get; }
+	/*---------------------------------- */
+	public Animator _animator;
+	/*----------------------------------- */
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	public void clickGainUnit()
+	{
+		units +=1;
+		if(units==5){
+			_animator.SetBool("PopUp", true);
+		}
+	}
+
+}
